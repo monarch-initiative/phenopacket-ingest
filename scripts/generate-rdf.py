5e85dd6eb6af0255ec0d3d52aa08da6761a3e701
iQuxLE <ckroll95@gmail.com>
2025-04-01 16:52:29 -0700

    add nodes and edges files to generate-rdf.py, update gh actions

--- scripts/generate-rdf.py
+++ scripts/generate-rdf.py
@@ -3,11 +3,11 @@ from pathlib import Path
 from kgx.cli.cli_utils import transform as kgx_transform
 from loguru import logger
 
-logger.info(f"Creating rdf output: output/data_source, ex. alliance, flybase, or hpoa._type_of_ingest, ex. gene, disease_to_phenotype, or disease_associations..nt.gz ...")
+logger.info("Creating rdf output: output/phenotype_ingest.nt.gz")
 
 src_files = []
-src_nodes = f"output/data_source, ex. alliance, flybase, or hpoa._type_of_ingest, ex. gene, disease_to_phenotype, or disease_associations._nodes.tsv"
-src_edges = f"output/data_source, ex. alliance, flybase, or hpoa._type_of_ingest, ex. gene, disease_to_phenotype, or disease_associations._edges.tsv"
+src_nodes = "output/phenopacket_ingest_nodes.tsv"
+src_edges = "output/phenopacket_ingest_edges.tsv"
 
 if Path(src_nodes).is_file():
     src_files.append(src_nodes)
@@ -18,7 +18,7 @@ kgx_transform(
     inputs=src_files,
     input_format="tsv",
     stream=True,
-    output=f"output/data_source, ex. alliance, flybase, or hpoa._type_of_ingest, ex. gene, disease_to_phenotype, or disease_associations..nt.gz",
+    output=f"output/phenotype_ingest.nt.gz",
     output_format="nt",
     output_compression="gz",
 )
